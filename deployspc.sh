798fa0716d61bac4f9ea6304fe0358fac6d4b598
Prasanna <prasannagardas9@gmail.com>
2024-11-11 22:39:07 +0530

    deploy

--- deployspc.sh
+++ deployspc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt install openjdk-17-jdk -y
-mkdir ~/apps 
+#mkdir ~/apps 
+cd ~/apps
 git clone https://github.com/Gitprasannag17/spring-pet-clinic-jar.git
 pkill -f tmux
-tmux new-session -d -s myapp 'java -jar ~/apps/spring-pet-clinic-jar/spring-petclinic-3.3.0-SNAPSHOT.jar'
+tmux new-session -d -s myapp 'java -jar /spring-pet-clinic-jar/spring-petclinic-3.3.0-SNAPSHOT.jar'
