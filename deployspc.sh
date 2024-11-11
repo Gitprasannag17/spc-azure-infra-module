8be88b7923547eceadecfac4f923e8fc2d7a2648
Prasanna <prasannagardas9@gmail.com>
2024-11-11 22:49:44 +0530

    deploy

--- deployspc.sh
+++ deployspc.sh
@@ -5,4 +5,5 @@
 cd ~/apps
 #git clone https://github.com/Gitprasannag17/spring-pet-clinic-jar.git
 #pkill -f tmux
-tmux new-session -d -s myapp 'java -jar /spring-pet-clinic-jar/spring-petclinic-3.3.0-SNAPSHOT.jar'
+#tmux new-session -d -s myapp 'java -jar /spring-pet-clinic-jar/spring-petclinic-3.3.0-SNAPSHOT.jar'
+java -jar /spring-pet-clinic-jar/spring-petclinic-3.3.0-SNAPSHOT.jar & sleep 60s
