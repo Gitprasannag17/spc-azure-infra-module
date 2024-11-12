9aefb3e0d72f1e09ff5ca26b349044832f736cc1
Prasanna <prasannagardas9@gmail.com>
2024-11-12 11:33:25 +0530

    deploy build jar

--- deployspc.sh
+++ deployspc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt update
-sudo apt install openjdk-17-jdk -y
-mkdir ~/apps 
-sudo cp -r /tmp/spc.jar/ ~/apps
+#sudo apt update
+#sudo apt install openjdk-17-jdk -y
+#mkdir ~/apps 
+sudo cp -r /tmp/spc.jar ~/apps
 cd ~/apps
 #git clone https://github.com/Gitprasannag17/spring-pet-clinic-jar.git
 pkill -f tmux
