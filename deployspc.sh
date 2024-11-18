2bb0ba76f6dc8921aa842a5d5ead88cb1dfd1ff1
Prasanna <prasannagardas9@gmail.com>
2024-11-18 17:43:00 +0530

    changed deployspc

--- deployspc.sh
+++ deployspc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo apt update
-#sudo apt install openjdk-17-jdk -y
-#mkdir ~/apps 
+sudo apt update
+sudo apt install openjdk-17-jdk -y
+mkdir ~/apps 
 sudo cp -r /tmp/spc.jar ~/apps/
 cd ~/apps
 #git clone https://github.com/Gitprasannag17/spring-pet-clinic-jar.git
